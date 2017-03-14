2fd743f4cfb9569fa2c7d55d3edc70dca9c33b16
Steven <Steven.c.hunt@gmail.com>
2017-03-14 16:01:15 -0700

    Add and Subtract

--- AddSub.asm
+++ AddSub.asm
@@ -1,14 +1,11 @@
-TITLE Add and Subtract, Version 2         (AddSub2.asm)
+TITLE Add and Subtract
 
-; This program adds and subtracts 32-bit integers
-; and stores the sum in a variable.
-
-; Swapped eax register with ebx.
-; Added val4 of 30000h to total
+; This program adds and subtracts 32-bit integer and stores the sum in a variable.
 
 INCLUDE Irvine32.inc
 
 .data
+
 val1     dword  10000h
 val2     dword  40000h
 val3     dword  20000h
