6f38ef55963a464457d9e0a4df2dd9dd278396ca
Steven <Steven.c.hunt@gmail.com>
2017-03-14 16:23:03 -0700

    Pointers and TYPEDEF

--- 8-Pointers_TYPEDEF.asm
+++ 8-Pointers_TYPEDEF.asm
@@ -7,9 +7,10 @@ INCLUDE Irvine32.inc
 ; Create user-defined types.
 PBYTE  TYPEDEF PTR BYTE		; pointer to bytes
 PWORD  TYPEDEF PTR WORD		; pointer to words
-PDWORD TYPEDEF PTR DWORD		; pointer to doublewords
+PDWORD TYPEDEF PTR DWORD	; pointer to doublewords
 
 .data
+
 arrayB BYTE  10h,20h,30h
 arrayW WORD  1,2,3
 arrayD DWORD 4,5,6
@@ -20,13 +21,16 @@ ptr2 PWORD  arrayW
 ptr3 PDWORD arrayD
 
 .code
+
 main PROC
 
-; Use the pointers to access data.
+	; Use the pointers to access data.
 	mov esi,ptr1
 	mov al,[esi]		; 10h
+	
 	mov esi,ptr2
 	mov ax,[esi]		; 1
+	
 	mov esi,ptr3
 	mov eax,[esi]		; 4
 
